e05f88b2f83db5fe8ceb3c398f16779057d45d82
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-10-16 19:30:40 +0800

    chore: brought back cors

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import * as log from './lib/logger';
 import express from 'express';
 
 import router from './routes';
+import cors from 'cors'
 
 /**
  * Online ordering API Service.
  */
 
-
 const port = 5000;
 const app = express()
-
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
